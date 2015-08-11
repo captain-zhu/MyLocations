aa162d0d720168a6ff97f1fb0d8c52cced3efcd5
captain-zhu <shibaier@foxmail.com>
2015-08-11 09:11:37 +0800

    Initial Commit

---
+++ MyLocations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLocations
+//
+//  Created by zhu yongxuan on 15/8/11.
+//  Copyright (c) 2015年 zhu yongxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLocations/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MyLocations
+//
+//  Created by zhu yongxuan on 15/8/11.
+//  Copyright (c) 2015年 zhu yongxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
